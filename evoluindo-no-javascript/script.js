17cecbfa942bd0109e4d9f250822dada6d711995
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-07-14 11:36:58 -0300

    feat: inicia estrutura com exemplos de laços while e for
    Adiciona index.html básico e script.js com exemplos de loop usando while e for. O while incrementa x até 9, e o for repete de 0 até x (5).

---
+++ evoluindo-no-javascript/script.js
@@ -0,0 +1,20 @@
+// while = Enquanto.
+
+x = 0;
+
+while(x < 10){
+
+	document.write("</br> O valor do X é: " + x);
+
+	x++;
+}
+
+x = 5;
+
+document.write("</br></br></br> O X está valendo: " + x + "</br></br>")
+
+// for = Para.
+
+for(a = 0; a < x; a++){
+	document.write("</br> O valor do A é: " + a);
+}
