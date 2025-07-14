e2bb3024a65b755e849fe5e752eb8f71a6a23e18
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-07-14 14:25:57 -0300

    feat: adiciona exemplos de laços e estrutura switch com interação via prompt
    Adiciona loops while e for que exibem valores no HTML. Implementa função pedir() com estrutura switch que mostra mensagens conforme opção digitada pelo usuário. HTML atualizado com botão que aciona essa interação.

--- evoluindo-no-javascript/script.js
+++ evoluindo-no-javascript/script.js
@@ -1,4 +1,5 @@
 // while = Enquanto.
+document.write("While");
 
 x = 0;
 
@@ -11,10 +12,47 @@ while(x < 10){
 
 x = 5;
 
-document.write("</br></br></br> O X está valendo: " + x + "</br></br>")
+document.write("</br></br> O X está valendo: " + x + "</br></br>")
+
+
+
 
 // for = Para.
+document.write("For");
 
 for(a = 0; a < x; a++){
 	document.write("</br> O valor do A é: " + a);
+}
+
+
+
+
+//switch 
+document.write("</br></br></br>");
+document.write("Switch </br>");
+
+document.write("Escolha seu pedido: </br>");
+document.write("</br> 0 - Sorvete / 1 - Suco </br>");
+document.write("</br> 2 - Coca-cola / 3 - Água </br></br>");
+
+function pedir(){
+	x = prompt("O que deseja pedir?");
+
+	switch(x){
+		case "0":
+			alert("Você pediu sorvete");
+			break;
+		case "1":
+			alert("Você pediu um suco");
+			break;
+		case "2":
+			alert("Você pediu uma coca-cola");
+			break;	
+		case "3":
+			alert("Você pediu ágau gelada");
+			break;
+		default:
+			alert("Não temos esta opção!");
+			break;		
+	}
 }
