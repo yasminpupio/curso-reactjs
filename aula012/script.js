894f747adb23c081ea6f2805f9af8422ff10cc99
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-05 09:43:39 -0300

    feat: adicionar estruturas de repetição while e for no JavaScript

---
+++ aula012/script.js
@@ -0,0 +1,17 @@
+// While - Enquanto 
+
+x = 0
+
+while (x < 10) {
+    document.writeln('<br> O valor do X: ' + x)
+
+    x++
+}
+
+document.writeln('<br><br><br>')
+
+// For - Para
+
+for (a = 0; a < 10; a++) {
+    document.writeln('<br> O valor do A: ' + a)
+}
