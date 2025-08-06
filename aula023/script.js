dd3c7fc8a3d23f1d4eb73d1af69b019042715cba
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-06 16:37:33 -0300

    feat: adiciona função para somar múltiplos números usando reduce

---
+++ aula023/script.js
@@ -0,0 +1,10 @@
+function adicionar(...numeros) {
+    let total = numeros.reduce((total, proximo) => {
+        let soma = total + proximo
+        return soma
+    })
+
+    console.log(total)
+}
+
+adicionar(1, 2, 3, 4, 5, 6)
