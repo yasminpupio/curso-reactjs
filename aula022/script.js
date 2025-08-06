340c92ee7e7334bdf2fa2442f4697804167b4ae9
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-06 15:38:38 -0300

    feat: implementa manipulação de array com map, reduce e find

---
+++ aula022/script.js
@@ -0,0 +1,19 @@
+const lista = [1, 2, 3, 4, 5, 6]
+
+const novaLista = lista.map(function (item, index) {
+    return item + index
+})
+
+console.log(novaLista)
+
+const soma = lista.reduce(function (total, proximo) {
+    return total + proximo
+})
+
+console.log(soma)
+
+const find = lista.find(function (item) {
+    return item === 6
+})
+
+console.log(find)
