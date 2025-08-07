d43a8e70272f5c0d539c6f9aa1d3abf4bf6ae9c8
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-07 08:13:42 -0300

    feat: adiciona exemplos de includes, satartsWith e endsWith em String

---
+++ aula024/script.js
@@ -0,0 +1,11 @@
+// includes
+/*let nomes = ['Yasmin', 'Lucas', 'João']
+console.log(nomes.includes('Lucas'))*/
+
+// startstWith
+/*let nome = 'Matheus'
+console.log(nome.startsWith('M'))*/
+
+// endsWith
+let nome = 'Matheus'
+console.log(nome.endsWith('eus'))
