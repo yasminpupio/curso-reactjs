2c7397c71082506389db5554849ba8ae7e3ac08d
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-07 14:17:46 -0300

    refactor(componentes): migrar componentes funcionais para classes e remover Social
    refactor(componentes)!: migrar para componentes de classe e remover Social
    
    BREAKING CHANGE: componente Social foi removido, os links de redes sociais não são mais exibidos

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,39 +1,34 @@
-const Equipe = (props) => {
-  return (
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-      <Social fb={props.facebook} />
-      <hr />
-    </div>
-  )
+import { Component } from "react"
+
+class Equipe extends Component {
+  render() {
+    return (
+      <div>
+        <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
+        <hr />
+      </div>
+    )
+  }
 }
 
-const Sobre = (props) => {
-  return (
-    <div>
-      <h2>Olá sou o(a) {props.nome}</h2>
-      <h3>Cargo: {props.cargo}</h3>
-      <h3>Idade: {props.idade} anos</h3>
-    </div>
-  )
-}
-
-const Social = (props) => {
-  return (
-    <div>
-      <a href={props.fb}>Facebook </a>
-      <a>LinkedIn </a>
-      <a>Youtube </a>
-    </div>
-  )
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Olá sou o(a) {this.props.nome}</h2>
+        <h3>Cargo: {this.props.cargo}</h3>
+        <h3>Idade: {this.props.idade} anos</h3>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div>
       <h1>Conheça nossa equipe:</h1>
-      <Equipe nome='Yasmin' cargo='Programador' idade='21' facebook='https://google.com' />
-      <Equipe nome='Lucas' cargo='Designer' idade='18' />
+      <Equipe nome='Yasmin' cargo='Programador' idade='21' />
+      <Equipe nome='Lucas' cargo='Designer' idade='15' />
     </div>
   )
 }
