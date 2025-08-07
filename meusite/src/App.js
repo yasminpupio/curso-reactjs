f8a3ef3f47d31d9a8c99abdffd819cd174d69f6a
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-07 11:05:28 -0300

    refactor: extrair Equipe, Sobre e Social de BemVindo
    Refatorado o componente BemVindo em três novos componentes:
    Equipe, Sobre e Social. Essa mudança melhora a reutilização
    e organização do código, além de permitir passar mais props
    como cargo e link de Facebook.

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,8 +1,29 @@
-const BemVindo = (props) => {
+const Equipe = (props) => {
   return (
     <div>
-      <h2>Bem-vindo(a) {props.nome}</h2>
-      <h3>Tenho {props.idade} anos</h3>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
+      <Social fb={props.facebook} />
+      <hr />
+    </div>
+  )
+}
+
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Olá sou o(a) {props.nome}</h2>
+      <h3>Cargo: {props.cargo}</h3>
+      <h3>Idade: {props.idade} anos</h3>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={props.fb}>Facebook </a>
+      <a>LinkedIn </a>
+      <a>Youtube </a>
     </div>
   )
 }
@@ -10,9 +31,9 @@ const BemVindo = (props) => {
 function App() {
   return (
     <div>
-      Olá mundo!
-      <BemVindo nome='Yasmin' idade='21' />
-      <BemVindo nome='Lucas' idade='10'/>
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe nome='Yasmin' cargo='Programador' idade='21' facebook='https://google.com' />
+      <Equipe nome='Lucas' cargo='Designer' idade='18' />
     </div>
   )
 }
