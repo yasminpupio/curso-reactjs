041c6807d143c36fd8f8d8ed575d3b3539ce1b77
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-07 10:18:00 -0300

    feat: remove conteúdo e cria componente BemVindo

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+const BemVindo = (props) => {
+  return (
+    <div>
+      <h2>Bem-vindo(a) {props.nome}</h2>
+      <h3>Tenho {props.idade} anos</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Olá mundo!
+      <BemVindo nome='Yasmin' idade='21' />
+      <BemVindo nome='Lucas' idade='10'/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
