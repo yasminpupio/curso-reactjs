58412058f2e4b089d96cecd557a4f790c3b7276e
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-08 08:10:19 -0300

    feat: adiciona componente App com contador e botões

---
+++ contador/src/App.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      contador: 0
+    }
+
+    this.aumentar = this.aumentar.bind(this)
+    this.diminuir = this.diminuir.bind(this)
+  }
+
+  aumentar() {
+    let state = this.state
+    state.contador += 1
+    this.setState(state)
+  }
+
+  diminuir() {
+    let state = this.state
+    state.contador -= 1
+    this.setState(state)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h3>
+          <button onClick={this.diminuir}>-</button>
+          {this.state.contador}
+          <button onClick={this.aumentar}>+</button>
+        </h3>
+      </div>
+    )
+  }
+
+}
+
+export default App
