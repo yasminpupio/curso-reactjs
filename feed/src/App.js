3129c54e7d15ff38b697d98876957923d73a7aad
Yasmin Pupio <127440208+yasminpupio@users.noreply.github.com>
2025-08-15 17:35:13 -0300

    feat: implementa componente App com lista de feeds
    Renderiza lista de Feeds com curtidas e comentários.

--- feed/src/App.js
+++ feed/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Feed from "./components/feed";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      feed: [
+        { id: 1, username: 'Yasmin', curtidas: 10, comentarios: 2 },
+        { id: 2, username: 'Lucas', curtidas: 120, comentarios: 24 },
+        { id: 3, username: 'Amanda', curtidas: 30, comentarios: 12 },
+        { id: 4, username: 'Ricardo', curtidas: 1, comentarios: 0 }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.feed.map((item) => {
+          return (
+            <Feed key={item.id} username={item.username} curtidas={item.curtidas}
+              comentarios={item.comentarios} />
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default App
