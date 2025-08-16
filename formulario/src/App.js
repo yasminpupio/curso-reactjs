d10b16efda599fab2c0c862f112e66dfc9bf17b6
Yasmin Pupio <127440208+yasminpupio@users.noreply.github.com>
2025-08-15 23:14:52 -0300

    feat: adiciona formulário de cadastro com validação de campos
    Implementado formulário de cadastro de usuário usando componente de classe.
    - Inclui campos: nome, e-mail e senha
    - Adicionada validação simples para garantir que nenhum campo esteja vazio
    - Exibe mensagem de erro caso falte algum dado
    - Mostra os dados em um alert quando o cadastro é válido

--- formulario/src/App.js
+++ formulario/src/App.js
@@ -2,10 +2,54 @@ import { Component } from "react";
 
 class App extends Component {
 
-  render(){
-    return(
+  constructor(props) {
+    super(props)
+    this.state = {
+      nome: '',
+      email: '',
+      senha: '',
+      error: ''
+    }
+
+    this.cadastrar = this.cadastrar.bind(this)
+  }
+
+  cadastrar(event) {
+    const { nome, email, senha } = this.state
+
+    if (nome != '' && email != '' && senha != '') {
+      alert(`Nome: ${nome} \nE-mail: ${email} \nSenha: ${senha}`)
+    } else {
+      this.setState({ error: 'Ops! Parece que está faltando algo!' })
+    }
+
+    event.preventDefault()
+  }
+
+  render() {
+    return (
       <div>
-        
+        <h1>Novo Usuário</h1>
+        {this.state.error && <p>{this.state.error}</p>}
+
+        <form onSubmit={this.cadastrar}>
+          <label>Nome: </label>
+          <input type="text" value={this.state.nome}
+            onChange={(e) => this.setState({ nome: e.target.value })} />
+          <br />
+
+          <label>E-mail: </label>
+          <input type="email" value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })} />
+          <br />
+
+          <label>Senha: </label>
+          <input type="password" value={this.state.senha}
+            onChange={(e) => this.setState({ senha: e.target.value })} />
+          <br />
+
+          <button type="submit">Cadastrar</button>
+        </form>
       </div>
     )
   }
