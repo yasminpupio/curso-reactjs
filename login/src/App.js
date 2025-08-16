b32322e252e410ddf9f4f5f37cce438b22ba91a4
Yasmin Pupio <127440208+yasminpupio@users.noreply.github.com>
2025-08-15 21:25:03 -0300

    feat: adiciona formulário de login controlado com email, senha e sexo

--- login/src/App.js
+++ login/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      senha: '',
+      sexo: 'masculino',
+    }
+
+    this.trocaEmail = this.trocaEmail.bind(this)
+    this.trocaSexo = this.trocaSexo.bind(this)
+  }
+
+  trocaEmail(e) {
+    let valorDigitado = e.target.value
+    this.setState({ email: valorDigitado })
+  }
+
+  trocaSexo(e){
+    this.setState({sexo: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+
+        E-mail:
+        <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail} />
+        <br />
+
+        Senha:
+        <input type="password" name="senha" value={this.state.senha} onChange={(e) =>
+          this.setState({ senha: e.target.value })} />
+        <br />
+        
+        Sexo:
+        <select value={this.state.sexo} onChange={this.trocaSexo}>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
+
+        <div>
+          <h3>{this.state.email}</h3>
+          <h3>{this.state.senha}</h3>
+          <h3>{this.state.sexo}</h3>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
