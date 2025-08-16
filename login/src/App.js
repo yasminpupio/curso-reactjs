c27f1131645e2d9f2a81d31e84de34f5df682758
Yasmin Pupio <127440208+yasminpupio@users.noreply.github.com>
2025-08-15 23:32:05 -0300

    refactor: unifica estado do formulário em objeto único e ajusta select
    - Alterado gerenciamento do formulário para usar objeto único (form) no state.
    - Criada função genérica (dadosForm) para atualizar qualquer campo do form dinamicamente.
    - Ajustado campo <select> para funcionar corretamente usando atributo name="sexo".
    - Código mais organizado e preparado para expansão futura.

--- login/src/App.js
+++ login/src/App.js
@@ -5,22 +5,21 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      email: '',
-      senha: '',
-      sexo: 'masculino',
+      form: {
+        nome: '',
+        email: '',
+        senha: '',
+        sexo: ''
+      }
     }
 
-    this.trocaEmail = this.trocaEmail.bind(this)
-    this.trocaSexo = this.trocaSexo.bind(this)
+    this.dadosForm = this.dadosForm.bind(this)
   }
 
-  trocaEmail(e) {
-    let valorDigitado = e.target.value
-    this.setState({ email: valorDigitado })
-  }
-
-  trocaSexo(e){
-    this.setState({sexo: e.target.value})
+  dadosForm(e) {
+    let form = this.state.form
+    form[e.target.name] = e.target.value
+    this.setState({ form: form })
   }
 
   render() {
@@ -28,25 +27,29 @@ class App extends Component {
       <div>
         <h2>Login</h2>
 
+        Nome:
+        <input type="text" name="nome" value={this.state.form.nome}
+          onChange={this.dadosForm} />
+        <br />
+
         E-mail:
-        <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail} />
+        <input type="email" name="email" value={this.state.form.email} onChange={this.dadosForm} />
         <br />
 
         Senha:
-        <input type="password" name="senha" value={this.state.senha} onChange={(e) =>
-          this.setState({ senha: e.target.value })} />
+        <input type="password" name="senha" value={this.state.form.senha} onChange={this.dadosForm} />
         <br />
-        
+
         Sexo:
-        <select value={this.state.sexo} onChange={this.trocaSexo}>
+        <select value={this.state.form.sexo} name="sexo" onChange={this.dadosForm}>
           <option value="masculino">Masculino</option>
           <option value="feminino">Feminino</option>
         </select>
 
         <div>
-          <h3>{this.state.email}</h3>
-          <h3>{this.state.senha}</h3>
-          <h3>{this.state.sexo}</h3>
+          <h3>{this.state.form.email}</h3>
+          <h3>{this.state.form.senha}</h3>
+          <h3>{this.state.form.sexo}</h3>
         </div>
       </div>
     )
