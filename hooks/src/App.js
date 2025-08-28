18bedede4f93c39af4769d8d233937b54347bc72
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-28 15:44:33 -0300

    feat: adiciona aplicação de tarefas com persistência
    Implementada aplicação inicial de lista de tarefas utilizando React. O código permite adicionar novas tarefas, listar todas as tarefas adicionadas e persistir os dados no localStorage com useEffect, garantindo que as tarefas permaneçam salvas entre sessões.

---
+++ hooks/src/App.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+function App() {
+
+  const [tarefas, setTarefas] = useState(() => {
+    const tarefasStorage = localStorage.getItem('tarefas')
+    return tarefasStorage ? JSON.parse(tarefasStorage) : []
+  })
+
+  const [input, setInput] = useState('')
+
+  /*useEffect(() => {
+    const tarefasStorage = localStorage.getItem('tarefas')
+
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+  }, [])*/
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }, [tarefas])
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+    setInput('')
+  }
+
+  return (
+    <div>
+      <ul>{tarefas.map(tarefa => (
+        <li key={tarefa}>{tarefa}</li>
+      ))}</ul>
+
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+
+      <button type="button" onClick={handleAdd}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
