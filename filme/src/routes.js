7305b04023f2efd0db39a0a89307eaa4340c29e9
Yasmin Pupio <yasminpupiodev@gmail.com>
2025-08-29 16:30:44 -0300

    feat: adiciona header as rotas
    Adiciona o componente Header nas rotas

--- filme/src/routes.js
+++ filme/src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 
 function RoutesApp() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/filme/:id" element={<Filme />} />
